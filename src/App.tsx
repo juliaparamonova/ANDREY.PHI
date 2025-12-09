61ebfc807f7489b3da3302c04b453a75aef69105
juliaparamonova <jpkoord@gmail.com>
2025-12-09 20:23:50 +0200

    upd body section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import Body from './components/Body/Body';
 
 export default function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Body />
     </>
   );
 }
---
+++ src/components/Body/Body.tsx
@@ -0,0 +1,16 @@
+import css from './Body.module.css';
+import BodyPC from '../../img/BodyPC.png';
+
+export default function Body() {
+  return (
+    <section className={css.section_body}>
+      <div className={css.body_pct}>
+        <h1 className={css.title_body}>ANDREY.PHI</h1>
+        <picture>
+          <source srcSet={BodyPC} type="image/png" />
+          <img src={BodyPC} alt="Fotografie eines der Werke" />
+        </picture>
+      </div>
+    </section>
+  );
+}
